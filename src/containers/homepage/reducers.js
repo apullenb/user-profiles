d6cdab56fff564c3398733a94f89d16030efe099
Andrea Pullen <apullenb@gmail.com>
2021-03-19 14:31:59 -0500

    setup redux

---
+++ src/containers/homepage/reducers.js
@@ -0,0 +1,11 @@
+const defaultState = {
+
+}
+
+
+export default function homePageReducer(state = defaultState, action) {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
